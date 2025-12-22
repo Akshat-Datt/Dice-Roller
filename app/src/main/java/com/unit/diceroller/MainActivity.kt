d077c3fadb3fbba9f94846f7980cee77ea4ad859
Akshat-Datt <dattakshat31@gmail.com>
2025-12-22 23:27:04 +0530

    Replacing text with real images, and making dice roller look lika a dice roller

--- app/src/main/java/com/unit/diceroller/MainActivity.kt
+++ app/src/main/java/com/unit/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.unit.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -28,15 +29,20 @@ class MainActivity : ComponentActivity() {
 
         rollBtn.setOnClickListener {
             rollDice()
-            Toast.makeText(this, "Dice is rolling!", Toast.LENGTH_SHORT).show()
         }
     }
 
     private fun rollDice() {
-        val diceNumber : TextView = findViewById<TextView>(R.id.tvDiceNummber)
-
-        var numberRolled = Random.nextInt(6) + 1
-
-        diceNumber.text = numberRolled.toString()
+        val diceImage : ImageView = findViewById<ImageView>(R.id.ivDice)
+        val numberRolled = Random.nextInt(6) + 1
+        val rolledDice = when(numberRolled){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(rolledDice)
     }
 }
